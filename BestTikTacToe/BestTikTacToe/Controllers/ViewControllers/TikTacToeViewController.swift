d58d5f59f050261a1a117107de12e8087bda4db8
James Lea <jameslea@Jamess-MBP.localdomain>
2021-05-19 14:11:09 -0600

    file structure, outlets, and actions made

---
+++ BestTikTacToe/BestTikTacToe/Controllers/ViewControllers/TikTacToeViewController.swift
@@ -0,0 +1,63 @@
+//
+//  TikTacToeViewController.swift
+//  BestTikTacToe
+//
+//  Created by James Lea on 5/19/21.
+//
+
+import UIKit
+
+class TikTacToeViewController: UIViewController {
+    
+    // MARK: - Outlets
+    @IBOutlet weak var a1Button: UIButton!
+    @IBOutlet weak var a2Button: UIButton!
+    @IBOutlet weak var a3Button: UIButton!
+    @IBOutlet weak var b1Button: UIButton!
+    @IBOutlet weak var b2Button: UIButton!
+    @IBOutlet weak var b3Button: UIButton!
+    @IBOutlet weak var c1Button: UIButton!
+    @IBOutlet weak var c2Button: UIButton!
+    @IBOutlet weak var c3Button: UIButton!
+    @IBOutlet weak var winnerLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    // MARK: - Actions
+    @IBAction func a1ButtonPressed(_ sender: Any) {
+    }
+    
+    @IBAction func a2ButtonPressed(_ sender: Any) {
+    }
+    @IBAction func a3ButtonPressed(_ sender: Any) {
+    }
+    @IBAction func b1ButtonPressed(_ sender: Any) {
+    }
+    @IBAction func b2ButtonPressed(_ sender: Any) {
+    }
+    @IBAction func b3ButtonPressed(_ sender: Any) {
+    }
+    @IBAction func c1ButtonPressed(_ sender: Any) {
+    }
+    @IBAction func c2ButtonPressed(_ sender: Any) {
+    }
+    @IBAction func c3ButtonPressed(_ sender: Any) {
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
